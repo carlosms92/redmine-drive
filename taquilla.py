af450cbe213b10e40f825f55b876038ebfcb00d7
carlos <carlosms92@hotmail.com>
2018-10-11 13:02:44 +0200

    get issues from yesterday

--- taquilla.py
+++ taquilla.py
@@ -1,17 +1,20 @@
 from redminelib import Redmine
-import time
+from datetime import date, timedelta
 
 redmine = Redmine('https://taquilla.antevenio.com/', username='', password='')
 
 user = redmine.user.get('current',include=['memberships','groups'])
 userId = user.id
 
-date = time.strftime("%Y-%m-%d")
+yesterday = date.today() - timedelta(1)
+dateYesterday = yesterday.strftime("%Y-%m-%d")
+
+print(dateYesterday)
 
 issues = redmine.issue.filter(
 	assigned_to_id=userId,
 	status_id="*",
-	updated_on=date,
+	updated_on=dateYesterday,
 	sort='category:desc',
 	include=['relations','journals']
 )
@@ -20,3 +23,5 @@ for issue in issues:
 	#ticket - proyecto - titulo - pais
 	#print(list(issue))
 	print(issue.id, " - " , issue.custom_fields[0].value, " - " ,issue.project.name, " - " , issue.subject)
+
+#con issue.time_entries se saca el tiempo dedicado (te lo da por dias)
